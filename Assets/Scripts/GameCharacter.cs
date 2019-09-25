0c49848dcda5d18172c24d6d31807f2c90c17c2d
zhanglong Peng <zhanglongpeng@wustl.edu>
2019-09-24 22:59:02 -0500

    walkright
    right walking implemented

--- Assets/Scripts/GameCharacter.cs
+++ Assets/Scripts/GameCharacter.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameCharacter : MonoBehaviour
 {
+
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class GameCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += new Vector3(0, 0.2f, 0);
@@ -21,14 +27,15 @@ public class GameCharacter : MonoBehaviour
         {
             transform.position += new Vector3(0, -0.2f, 0);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(0.2f, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(-0.2f, 0, 0);
-        }
-
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.position += new Vector3(0.2f, 0, 0);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.position += new Vector3(-0.2f, 0, 0);
+        //}
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal*Time.deltaTime;
     }
 }
