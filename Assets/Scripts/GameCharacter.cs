03e3a9fcf47ace2062954ea3229bae8f9b9524aa
zhanglong Peng <zhanglongpeng@wustl.edu>
2019-10-01 20:53:09 -0500

    Revert "Revert "add art and tilemap and 2 scenes and colliders and change scene script""
    This reverts commit cbeb178df6ae36bedc25b25b0099b9a09f29f7f1.

--- Assets/Scripts/GameCharacter.cs
+++ Assets/Scripts/GameCharacter.cs
@@ -6,11 +6,27 @@ public class GameCharacter : MonoBehaviour
 {
 
     public Animator animator;
+    Rigidbody2D rigidbody;
 
+    //private static bool created = false;
+
+    ////code from documentation: https://docs.unity3d.com/ScriptReference/SceneManagement.Scene-name.html
+    //private void Awake()
+    //{
+    //    if (!created)
+    //    {
+    //        DontDestroyOnLoad(this.gameObject);
+    //        created = true;
+    //    }
+    //    else
+    //    {
+    //        Destroy(this.gameObject);
+    //    }
+
+    //}
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -18,7 +34,7 @@ public class GameCharacter : MonoBehaviour
     {
 
 
-        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += new Vector3(0, 0.2f, 0);
@@ -27,15 +43,16 @@ public class GameCharacter : MonoBehaviour
         {
             transform.position += new Vector3(0, -0.2f, 0);
         }
-        //if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    transform.position += new Vector3(0.2f, 0, 0);
-        //}
-        //if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    transform.position += new Vector3(-0.2f, 0, 0);
-        //}
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(0.2f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-0.2f, 0, 0);
+        }
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + horizontal*Time.deltaTime;
     }
+
 }
