c9928c3ccc15b3f83bf2044d067e243b67f5a26e
zijunzhao <z.zhao@wustl.edu>
2019-10-31 17:58:27 -0500

    changed something

--- Assets/Scripts/GameCharacter.cs
+++ Assets/Scripts/GameCharacter.cs
@@ -7,6 +7,8 @@ public class GameCharacter : MonoBehaviour
 
     public Animator animator;
     Rigidbody2D rigidbody;
+    public GameObject projectilePrefab;
+
     private SpriteRenderer sprite;
 
 
@@ -44,6 +46,13 @@ public class GameCharacter : MonoBehaviour
             transform.position += new Vector3(-0.2f, 0, 0);
         }
 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            GameObject newProjectile = Instantiate(projectilePrefab);
+            newProjectile.transform.position = transform.position;
+            newProjectile.transform.rotation = transform.rotation;
+        }
+
         //send values to animator
         animator.SetBool("WalkRight", Input.GetKey(KeyCode.RightArrow));
         animator.SetBool("WalkLeft", Input.GetKey(KeyCode.LeftArrow));
@@ -51,16 +60,6 @@ public class GameCharacter : MonoBehaviour
         animator.SetBool("WalkUp", Input.GetKey(KeyCode.UpArrow));
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
-
-        //Debug.Log(animator.GetCurrentAnimatorStateInfo(0).IsName("RunDown"));
-        
-        
-
-
-        //Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        //transform.position = transform.position + horizontal*Time.deltaTime;
-        //Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
-        //transform.position = transform.position + vertical*Time.deltaTime;
 
 
 
