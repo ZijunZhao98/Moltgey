eaf282015f0bc53d10acb3f93b5c41372771c508
ZijunZhao98 <zijun_zhao1998@163.com>
2019-12-01 22:18:56 -0600

    ui change

---
+++ Assets/Scripts/Font.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class Font: MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Text>().font.material.mainTexture.filterMode = FilterMode.Point;
+    }
+}
---
+++ Assets/Scripts/VendingController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VendingController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int count = 5;
+    public Text cokeNum;
+    public GameObject vm;
+
+    public void Awake()
+    {
+        vm.SetActive(false);
+    }
+
+    public void BuyCoke()
+    {
+        if(count > 0)
+        {
+            count--;
+            cokeNum.text = count.ToString();
+        }
+    }
+
+    public void Show()
+    {
+        vm.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        vm.SetActive(false);
+    }
+}
+
+
